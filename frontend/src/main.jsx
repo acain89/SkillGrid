eab035046db0f437afd0deb26fa2f01e2b7aa837
Andrew Cain <acain89@gmail.com>
2025-12-07 05:52:22 -0600

    Auto-sync update

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { AuthProvider } from "./context/AuthContext";
+import "./services/firebase.js";
+
 
 // 🚫 NO React.StrictMode (Google popup breaks under StrictMode)
 ReactDOM.createRoot(document.getElementById("root")).render(
